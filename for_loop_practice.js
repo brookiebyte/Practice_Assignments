5614bdf534bf607489bdd3f2a86abc405e27e06b
brookiebyte <brookie.darby@gmail.com>
2024-10-19 15:31:35 -0600

    it works now

--- for_loop_practice.js
+++ for_loop_practice.js
@@ -1,57 +1,57 @@
-class Practice {
-    constructor() {}
-    // 1. Create a function that prints the numbers from 1 to 10
-    oneToTen() {
 
+// 1. Create a function that prints the numbers from 1 to 10
+function oneToTen() {
+    for (let k = 1; k <= 10; k = k + 1 ) {
+        console.log(k)
     }
+}
 
-    // 2. Create a function that prints numbers 10 to 1
-    tenToOne() {
+// 2. Create a function that prints numbers 10 to 1
+function tenToOne() {
 
-    }
+}
 
-    // 3. create a function that prints every element of an input array in order
-    printInOrder(nums) {
+// 3. create a function that prints every element of an input array in order
+function printInOrder(nums) {
 
-    }
+}
 
-    // 4. Create a function that prints every element of an array in reverse order
-    printReverseOrder(nums) {
+// 4. Create a function that prints every element of an array in reverse order
+function printReverseOrder(nums) {
 
-    }
+}
 
-    // 5. Create a function that prints even numbers from 1 to 10
-    printEvenToTen() {
+// 5. Create a function that prints even numbers from 1 to 10
+function printEvenToTen() {
 
-    }
+}
 
-    // 6. Create a function that prints odd numbers from 1 to 10
-    printOddToTen() {
+// 6. Create a function that prints odd numbers from 1 to 10
+function printOddToTen() {
 
-    }
+}
+
+// 7. Create a function that prints every other element of an input array
+function printEveryOtherElement(nums) {
+
+}
+
+// 8. Create a function that prints the first half of an input array
+function printFirstHalf(nums) {
+
+}
+
+// 9. Create a function that prints the last half of an input array
+function printLastHalf(nums) {
+
+}
+
+// 10. Create a function that prints the even numbers in an array
+function printEvens(nums) {
+
+}
+
+// 11. Create a function that prints odd numbers in an array
+function printOdds(nums) {
     
-    // 7. Create a function that prints every other element of an input array
-    printEveryOtherElement(nums) {
-
-    }
-    
-    // 8. Create a function that prints the first half of an input array
-    printFirstHalf(nums) {
-
-    }
-
-    // 9. Create a function that prints the last half of an input array
-    printLastHalf(nums) {
-
-    }
-
-    // 10. Create a function that prints the even numbers in an array
-    printEvens(nums) {
-
-    }
-
-    // 11. Create a function that prints odd numbers in an array
-    printOdds(nums) {
-        
-    }
 }
