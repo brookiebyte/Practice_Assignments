ac3d11239f6715fdb41f9e0a6a39ee7a80759efe
sethpolevoi <sethpolevoi2@gmail.com>
2024-10-19 14:52:58 -0600

    assignment might not compile

---
+++ for_loop_practice.js
@@ -0,0 +1,57 @@
+class Practice {
+    constructor() {}
+    // 1. Create a function that prints the numbers from 1 to 10
+    oneToTen() {
+
+    }
+
+    // 2. Create a function that prints numbers 10 to 1
+    tenToOne() {
+
+    }
+
+    // 3. create a function that prints every element of an input array in order
+    printInOrder(nums) {
+
+    }
+
+    // 4. Create a function that prints every element of an array in reverse order
+    printReverseOrder(nums) {
+
+    }
+
+    // 5. Create a function that prints even numbers from 1 to 10
+    printEvenToTen() {
+
+    }
+
+    // 6. Create a function that prints odd numbers from 1 to 10
+    printOddToTen() {
+
+    }
+    
+    // 7. Create a function that prints every other element of an input array
+    printEveryOtherElement(nums) {
+
+    }
+    
+    // 8. Create a function that prints the first half of an input array
+    printFirstHalf(nums) {
+
+    }
+
+    // 9. Create a function that prints the last half of an input array
+    printLastHalf(nums) {
+
+    }
+
+    // 10. Create a function that prints the even numbers in an array
+    printEvens(nums) {
+
+    }
+
+    // 11. Create a function that prints odd numbers in an array
+    printOdds(nums) {
+        
+    }
+}
--- script.js
+++ script.js
@@ -20,8 +20,11 @@ function fetchJSONFile (path, callback) {
 
 // call fetchJSONFile then build and render a tree
 // this is the function executed as a callback when parsing is done
-fetchJSONFile('data/Tree.json', function (data) {
-  const tree = new Tree(data);
-  tree.buildTree();
-  tree.renderTree();
+fetchJSONFile('for_loop_practice.json', function (data) {
+  const practice = new Practice();
+  practice.oneToTen();
+  practice.oneToTen();
+  const mySmallArray = [1,2,3,4,5,6,7,8,9,0];
+  const myLongArray = [27, 5, 83, 12, 54, 73, 91, 39, 66, 7, 25, 48, 16, 34, 58, 69, 11, 94, 22, 18, 29, 81, 3, 40, 65, 78];
+  
 });
